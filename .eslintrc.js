0c99edb32776e182fab15170eada50a83838e704
Patryk Bernasiewicz <patryk.bernasiewicz@netguru.com>
2021-05-20 12:06:13 +0200

    add eslint, prettier, typescript, fix issues

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint",
+    "react-hooks",
+  ],
+  "rules": {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["error", { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    "react/jsx-one-expression-per-line": ["error", { allow: 'literal' }],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "import/extensions": "off",
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {},
+    },
+  },
+};
