45132ef0311a8bdf79671e36dc04fb1284b8335a
Patryk Bernasiewicz <patryk.bernasiewicz@netguru.com>
2021-05-25 09:11:01 +0200

    fix things broken during tests

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,7 +1,6 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme {
     color: {
       white: string;
